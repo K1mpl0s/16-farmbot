6ff0ade16feb781f95d62f14f15cd9e1e524acb9
K1mpl0s <45961928+K1mpl0s@users.noreply.github.com>
2021-07-11 23:52:04 -0400

    Update android.py

--- android.py
+++ android.py
@@ -4,9 +4,6 @@ import requests
 import os
 import subprocess
 
-r = requests.get(f"https://raw.githubusercontent.com/K1mpl0s/16-farmbot/main/version.json")
-version = r.json()['version']
-
 if '3.7' not in str(sys.version):
     uname = platform.machine()
     clean_uname = None
@@ -25,28 +22,42 @@ if '3.7' not in str(sys.version):
     f.close()
     subprocess.run(f"dpkg -i ./python_3.7.5_{uname}.deb", shell=True, check=False)
     print('\n\n')
+    subprocess.run(f"pip3 install six colorama requests pyCryptodomex==3.9.9 lxml bs4 werkzeug", shell=True, check=False)
+    print('\n\n')
     print('[!] successfully installed python3.7')
 dir_path = os.path.dirname(os.path.realpath(__file__))
 print(dir_path)
+folder_found = False
 if os.path.isdir('./storage/'):
     if os.path.isdir('./storage/downloads/'):
-        if os.path.isdir('./storage/downloads/16-' + version):
-            if os.path.isdir('./storage/downloads/16-' + version + '/16 Farmbot/'):
-                if os.path.isdir('./storage/downloads/16-' + version + '/16 Farmbot/source/'):
+        folders = sorted(os.listdir('./storage/downloads/'), key=os.path.getmtime, reverse=True)
+        for i in folders:
+            if i == '16 Farmbot':
+                os.chdir(f"./storage/downloads/16 Farmbot/")
+                if os.path.isdir('./source/'):
+                    folder_found = True
                     wd = os.getcwd()
-                    os.chdir(f"./storage/downloads/16-{version}/16 Farmbot/source/")
+                    os.chdir(f"./storage/downloads/16 Farmbot/source/")
                     subprocess.run(f"python3 bot.pyc", shell=True, check=False)
                 else:
-                    print('[!] unable to locate "source" folder in "16 Farmbot"')
-            else:
-                print('[!] unable to locate "16 Farmbot" folder in "downloads"')
-        elif os.path.isdir('./storage/downloads/16 Farmbot/source/'):
-            wd = os.getcwd()
-            os.chdir(f"./storage/downloads/16 Farmbot/source/")
-            subprocess.run(f"python3 bot.pyc", shell=True, check=False)
-        else:
-            print('[!] unable to locate 16 folder in "downloads"')
+                    print('[!] unable to locate source in "16 Farmbot"')
+                break
+            elif '16-' in i:
+                os.chdir(f"./storage/downloads/{i}/")
+                if os.path.isdir(f"./16 Farmbot/"):
+                    if os.path.isdir('./storage/downloads/16-' + version + '/16 Farmbot/source/'):
+                        folder_found = True
+                        wd = os.getcwd()
+                        os.chdir(f"./storage/downloads/16-{version}/16 Farmbot/source/")
+                        subprocess.run(f"python3 bot.pyc", shell=True, check=False)
+                    else:
+                        print('[!] unable to locate "source" folder in "16 Farmbot"')
+                else:
+                    print('[!] unable to locate "16 Farmbot" folder in 16 Folder')
+                break
+        if not folder_found:
+            print('[!] unable to locate 16 Folder in "downloads"')
     else:
-        print('[!] unable to locate "storage/downloads"')
+        print('[!] unable to locate "downloads"')
 else:
     print('[!] unable to locate "storage"\n...make sure you ran "termux-setup-storage"')
